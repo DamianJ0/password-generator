3054ff9648b418c60f8d48fa198af27f6b8f8435
Damian Jaworowski <damianjaworowski0@gmail.com>
2023-03-28 23:04:56 +0200

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,9 +8,45 @@ function Home() {
         <h2 className='title'>Password Generator</h2>
         <div className="password-wrapper">
             <div className="password-area">
-                <div className="className">
+                <div className="password">
                     <input type="text" disabled/>
                     <img src={copyIcon} alt='copyIcon' className='copyIcon' />
+                </div>
+            </div>
+        </div>
+        <div className="setting">
+            <h3>Customize your password</h3>
+            <div className="customize">
+                <div className="checkboxes">
+                    <div className="left">
+                        <div className="checkbox-field">
+                        <input type="checkbox" name="lower" id="lower" />
+                        <label htmlFor='lower'>Include lower case(a-z)</label>
+                        </div>
+                        <div className="checkbox-field">
+                        <input type="checkbox" name="upper" id="upper" />
+                        <label htmlFor='upper'>Include upper case(A-Z)</label>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <div className="checkbox-field">
+                        <input type="checkbox" name="numbers" id="numbers" />
+                        <label htmlFor='numbers'>Include numbers(0-9)</label>
+                        </div>
+                        <div className="checkbox-field">
+                        <input type="checkbox" name="symbols" id="symbols" />
+                        <label htmlFor='symbols'>Include symbols(!-*)</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="password-length">
+            <h3>Password Length</h3>
+            <div className="slider">
+                <p className="rangeValue">10</p>
+                <div className="range">
+                    <input type="range" min={10} max={30} />
                 </div>
             </div>
         </div>
